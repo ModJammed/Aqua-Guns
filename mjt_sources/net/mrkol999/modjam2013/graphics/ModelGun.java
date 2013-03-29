88b21465fe325b89a9dc4eac63987950b00dfced
Roman Sandu <roma.sandu@gmail.com>
2013-03-29 21:13:16 +0400

    Liquid mechanic - done. Liquid rendering - in progress.

--- mjt_sources/net/mrkol999/modjam2013/graphics/ModelGun.java
+++ mjt_sources/net/mrkol999/modjam2013/graphics/ModelGun.java
@@ -1,12 +1,16 @@
 package net.mrkol999.modjam2013.graphics;
 
 import org.lwjgl.opengl.GL11;
+import net.minecraft.block.Block;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.liquids.IBlockLiquid;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.src.ModLoader;
+import net.minecraftforge.liquids.LiquidDictionary;
 import net.minecraftforge.liquids.LiquidStack;
 import net.minecraft.client.renderer.Tessellator;
 
@@ -42,49 +46,61 @@ public class ModelGun extends ModelBase
 
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5, ItemStack is)
 	{
+    	LiquidStack currls;
+    	try
+    	{
+    		currls = LiquidStack.loadLiquidStackFromNBT(is.getTagCompound().getCompoundTag("LiquidData"));
+    	}
+    	catch(Exception e)
+    	{
+    		currls = null;
+    	}
+    	
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 		Pice0.render(f5);
-		if(is.getItemDamage() < 1000)
+		if(currls != null && currls.itemID != 0 && currls.amount > 0)
 		{
-			GL11.glBindTexture(GL11.GL_TEXTURE_2D, ModLoader.getMinecraftInstance().renderEngine
-					.getTexture(Item.itemsList[LiquidStack.loadLiquidStackFromNBT(is.getTagCompound().getCompoundTag(
-							"LiquidData")).itemID].getSpriteNumber() == 1 ? "/gui/items.png" : "/terrain.png"));
+			String tex = "";
+			if(Block.blocksList[currls.itemID] instanceof IBlockLiquid) tex = ((IBlockLiquid)Block.blocksList[currls.itemID]).getLiquidBlockTextureFile();
+			else tex = "/terrain.png";
+			
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, ModLoader.getMinecraftInstance().renderEngine.getTexture(tex));
 
-			Tessellator tes = Tessellator.instance; //Eclipse auto formatter FTW
+			Tessellator tes = Tessellator.instance;
 
 			int miu = (int) is.getItem().getIconIndex(is).getMinU();
 			int miv = (int) is.getItem().getIconIndex(is).getMinV();
 			int mau = (int) is.getItem().getIconIndex(is).getMaxU();
 			int mav = (int) is.getItem().getIconIndex(is).getMaxV();
-			int lh = (int) Math.floor(((is.getItemDamage() * -1) + 1000) / 5);
+			int lh = (int) Math.floor(currls.amount / 200);
 
-			tes.startDrawingQuads(); // brutal tessellation code
+			tes.startDrawingQuads(); // ugly tessellation code
 			{
 				tes.addVertexWithUV(-1.5, -3, -1.5, mau, mav);
 				tes.addVertexWithUV(1.5, -3, -1.5, mau, miv);
-				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, miu, mav);
-				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, miv);
+				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, mav);
+				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, mau, mav);
 
 				tes.addVertexWithUV(-1.5, -3, -1.5, mau, mav);
 				tes.addVertexWithUV(-1.5, -3, 1.5, mau, miv);
-				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, miu, mav);
-				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, miu, miv);
+				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, miu, mav);
+				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, mau, mav);
 
 				tes.addVertexWithUV(-1.5, -3, 1.5, mau, mav);
 				tes.addVertexWithUV(1.5, -3, 1.5, mau, miv);
-				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, miu, mav);
-				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, miv);
+				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, mav);
+				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, mau, mav);
 
 				tes.addVertexWithUV(1.5, -3, -1.5, mau, mav);
 				tes.addVertexWithUV(1.5, -3, 1.5, mau, miv);
-				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, mav);
-				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, miv);
+				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, mav);
+				tes.addVertexWithUV(1.5, -3 - lh, -1.5, mau, mav);
 
 				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, mau, mav);
 				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, mau, miv);
-				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, mav);
-				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, miv);
+				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, mav);
+				tes.addVertexWithUV(1.5, -3 - lh, -1.5, mau, mav);
 			}
 			tes.draw();
 		}
--- mjt_sources/net/mrkol999/modjam2013/items/ItemGun.java
+++ mjt_sources/net/mrkol999/modjam2013/items/ItemGun.java
@@ -1,67 +1,157 @@
 package net.mrkol999.modjam2013.items;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockFluid;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumMovingObjectType;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
+import net.minecraftforge.liquids.IBlockLiquid;
+import net.minecraftforge.liquids.LiquidDictionary;
+import net.minecraftforge.liquids.LiquidStack;
 
-public class ItemGun extends Item //remember how all this stuff work in new forge.
+public class ItemGun extends Item
 {
 	public ItemGun(int par1)
 	{
 		super(par1);
 		this.setMaxStackSize(1);
-		this.setCreativeTab(CreativeTabs.tabCombat); // epic combat gun is in the epic combat tab is creative
+		this.setCreativeTab(CreativeTabs.tabCombat); // epic combat gun is in
+														// the epic combat tab
+														// is creative
 		this.setNoRepair();
 		this.setUnlocalizedName("aquagun");
-        this.setMaxDamage(1000); //1000 millibuckets = 1 bucket
+		this.setMaxDamage(1000); // 1000 millibuckets = 1 bucket
 	}
-	
+
 	@Override
-    public EnumAction getItemUseAction(ItemStack par1ItemStack)
-    {
-        return EnumAction.bow;
-    }
-    
-    @Override
-    public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
-    {
-        return par1ItemStack;
-    }
-    
-    @Override
-    public int getMaxItemUseDuration(ItemStack par1ItemStack)
-    {
-        return 36000;
-    }
-    
-    @Override
-    public int getItemEnchantability()
-    {
-        return 0; //nope, no enchants
-    }
-    
-    @Override
-    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
-    {
-        if (par3EntityPlayer.capabilities.isCreativeMode || par1ItemStack.getItemDamage() < 1000)
-        {
-            par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
-        }
+	public EnumAction getItemUseAction(ItemStack par1ItemStack)
+	{
+		return EnumAction.bow;
+	}
 
-        return par1ItemStack;
-    }
-    
-    @Override
-    public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4)
-    {
-        boolean flag = par3EntityPlayer.capabilities.isCreativeMode;
+	@Override
+	public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+	{
+		return par1ItemStack;
+	}
 
-        if (flag || par1ItemStack.getItemDamage() < 1000) //if gamemode is creative, or if we still have some liquid left...
-        {
-            par1ItemStack.damageItem(1, par3EntityPlayer);
-        }
-    }
+	@Override
+	public int getMaxItemUseDuration(ItemStack par1ItemStack)
+	{
+		return 36000;
+	}
+
+	@Override
+	public int getItemEnchantability()
+	{
+		return 0; // nope, no enchants
+	}
+
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+	{
+        MovingObjectPosition mop = this.getMovingObjectPositionFromPlayer(par2World, par3EntityPlayer, true);
+        Block block = null;
+        if(mop != null && mop.typeOfHit == EnumMovingObjectType.TILE) block = Block.blocksList[par2World.getBlockId(mop.blockX, mop.blockY, mop.blockZ)];
+		LiquidStack currls;
+		try
+		{
+			currls = LiquidStack.loadLiquidStackFromNBT(par1ItemStack.getTagCompound().getCompoundTag("LiquidData"));
+		}
+		catch(Exception e)
+		{
+			if(block != null && (block instanceof IBlockLiquid || block instanceof BlockFluid))
+			{
+				if(block instanceof IBlockLiquid) currls = LiquidDictionary.getLiquid(((IBlockLiquid)block).getLiquidProperties().getName(), 1000);
+				else
+				{
+					currls = LiquidDictionary.getLiquid(block.blockID == Block.waterStill.blockID || block.blockID == Block.waterMoving.blockID ? "Water" : "Lava", 1000);
+				}
+				NBTTagCompound nbt = new NBTTagCompound();
+				
+				currls.writeToNBT(nbt);
+	
+				if(par1ItemStack.getTagCompound() == null) par1ItemStack.stackTagCompound = new NBTTagCompound();
+	
+				par1ItemStack.getTagCompound().setTag("LiquidData", nbt);
+			}
+			else
+			{
+				currls = null;
+			}
+		}
+		
+		if(block != null && (block instanceof IBlockLiquid || block instanceof BlockFluid))
+		{
+			if(currls.amount < 1000)
+			{
+				if(block.blockID == currls.itemID)
+				{
+					currls.amount = 1000;
+					par2World.setBlock(mop.blockX, mop.blockY, mop.blockZ, 0);
+				}
+				else
+				if(currls.amount == 0)
+				{
+					if(block instanceof IBlockLiquid) currls = LiquidDictionary.getLiquid(((IBlockLiquid)block).getLiquidProperties().getName(), 1000);
+					else
+					{
+						currls = LiquidDictionary.getLiquid(block.blockID == Block.waterStill.blockID || block.blockID == Block.waterMoving.blockID ? "Water" : "Lava", 1000);
+					}
+					par2World.setBlock(mop.blockX, mop.blockY, mop.blockZ, 0);
+				}
+				
+				NBTTagCompound nbt = new NBTTagCompound();
+	
+				currls.writeToNBT(nbt);
+	
+				if(par1ItemStack.getTagCompound() == null) par1ItemStack.stackTagCompound = new NBTTagCompound();
+	
+				par1ItemStack.getTagCompound().setTag("LiquidData", nbt);
+			}
+		}
+		else
+		{
+			if(par3EntityPlayer.capabilities.isCreativeMode || currls.amount > 0)
+			{
+				par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+			}
+		}
+		
+		
+		
+		return par1ItemStack;
+	}
+
+	@Override
+	public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4)
+	{
+		if(!par2World.isRemote)
+		{
+			boolean flag = par3EntityPlayer.capabilities.isCreativeMode;
+	
+			LiquidStack currls = LiquidStack.loadLiquidStackFromNBT(par1ItemStack.getTagCompound().getCompoundTag("LiquidData"));
+	
+			if(flag || currls.amount > 0) // if gamemode is creative, or if we still have any liquid left...
+			{
+				if(!flag) currls.amount--;
+				par3EntityPlayer.addChatMessage("PEW! The gun fires: " + currls.itemID + "; " + currls.amount + "; ");
+				// launch da liquid missile, pew.
+			}
+	
+			NBTTagCompound nbt = new NBTTagCompound();
+	
+			currls.writeToNBT(nbt);
+	
+			if(par1ItemStack.getTagCompound() == null) par1ItemStack.stackTagCompound = new NBTTagCompound();
+	
+			par1ItemStack.getTagCompound().setTag("LiquidData", nbt);
+		}
+	}
 }
