91f44214270c6415a08ca35036513451d00e227a
Roman Sandu <roma.sandu@gmail.com>
2013-03-29 17:37:58 +0400

    Liquid storing mechanic progress.

--- mjt_sources/net/mrkol999/modjam2013/graphics/ModelGun.java
+++ mjt_sources/net/mrkol999/modjam2013/graphics/ModelGun.java
@@ -1,27 +1,32 @@
 package net.mrkol999.modjam2013.graphics;
 
+import org.lwjgl.opengl.GL11;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.src.ModLoader;
+import net.minecraftforge.liquids.LiquidStack;
+import net.minecraft.client.renderer.Tessellator;
 
 public class ModelGun extends ModelBase
 {
-	ModelRenderer Pice0;
-	
+	ModelRenderer Pice0; // The gun
+
 	public ModelGun()
 	{
 		textureWidth = 68;
 		textureHeight = 68;
 		setTextureOffset("Pice0.Part0", 0, 22);
 		setTextureOffset("Pice0.Part1", 0, 17);
-		setTextureOffset("Pice0.Part2", 0, 12); //hope techne done it right
+		setTextureOffset("Pice0.Part2", 0, 12); // hope techne done it right
 		setTextureOffset("Pice0.Part3", 0, 27);
 		setTextureOffset("Pice0.Part4", 0, 0);
 		setTextureOffset("Pice0.Part5", 56, 0);
 		setTextureOffset("Pice0.Part6", 0, 32);
 		setTextureOffset("Pice0.Part7", 24, 0);
-		
+
 		Pice0 = new ModelRenderer(this, "Pice0");
 		Pice0.setRotationPoint(0F, 0F, 0F);
 		Pice0.mirror = false;
@@ -34,21 +39,57 @@ public class ModelGun extends ModelBase
 		Pice0.addBox("Part6", 14F, -3F, -3F, 1, 6, 6);
 		Pice0.addBox("Part7", -2F, -9F, -2F, 4, 6, 4);
 	}
-	
+
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5, ItemStack is)
 	{
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 		Pice0.render(f5);
+		if(is.getItemDamage() < 1000)
+		{
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, ModLoader.getMinecraftInstance().renderEngine
+					.getTexture(Item.itemsList[LiquidStack.loadLiquidStackFromNBT(is.getTagCompound().getCompoundTag(
+							"LiquidData")).itemID].getSpriteNumber() == 1 ? "/gui/items.png" : "/terrain.png"));
+
+			Tessellator tes = Tessellator.instance; //Eclipse auto formatter FTW
+
+			int miu = (int) is.getItem().getIconIndex(is).getMinU();
+			int miv = (int) is.getItem().getIconIndex(is).getMinV();
+			int mau = (int) is.getItem().getIconIndex(is).getMaxU();
+			int mav = (int) is.getItem().getIconIndex(is).getMaxV();
+			int lh = (int) Math.floor(((is.getItemDamage() * -1) + 1000) / 5);
+
+			tes.startDrawingQuads(); // brutal tessellation code
+			{
+				tes.addVertexWithUV(-1.5, -3, -1.5, mau, mav);
+				tes.addVertexWithUV(1.5, -3, -1.5, mau, miv);
+				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, miu, mav);
+				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, miv);
+
+				tes.addVertexWithUV(-1.5, -3, -1.5, mau, mav);
+				tes.addVertexWithUV(-1.5, -3, 1.5, mau, miv);
+				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, miu, mav);
+				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, miu, miv);
+
+				tes.addVertexWithUV(-1.5, -3, 1.5, mau, mav);
+				tes.addVertexWithUV(1.5, -3, 1.5, mau, miv);
+				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, miu, mav);
+				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, miv);
+
+				tes.addVertexWithUV(1.5, -3, -1.5, mau, mav);
+				tes.addVertexWithUV(1.5, -3, 1.5, mau, miv);
+				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, mav);
+				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, miv);
+
+				tes.addVertexWithUV(-1.5, -3 - lh, -1.5, mau, mav);
+				tes.addVertexWithUV(-1.5, -3 - lh, 1.5, mau, miv);
+				tes.addVertexWithUV(1.5, -3 - lh, -1.5, miu, mav);
+				tes.addVertexWithUV(1.5, -3 - lh, 1.5, miu, miv);
+			}
+			tes.draw();
+		}
 	}
-	
-	private void setRotation(ModelRenderer model, float x, float y, float z)
-	{
-		model.rotateAngleX = x;
-		model.rotateAngleY = y;
-		model.rotateAngleZ = z;
-	}
-	
+
 	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
 	{
 		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
--- mjt_sources/net/mrkol999/modjam2013/items/ItemGun.java
+++ mjt_sources/net/mrkol999/modjam2013/items/ItemGun.java
@@ -1,9 +1,11 @@
 package net.mrkol999.modjam2013.items;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemGun extends Item //remember how all this stuff work in new forge.
 {
@@ -14,10 +16,52 @@ public class ItemGun extends Item //remember how all this stuff work in new forg
 		this.setCreativeTab(CreativeTabs.tabCombat); // epic combat gun is in the epic combat tab is creative
 		this.setNoRepair();
 		this.setUnlocalizedName("aquagun");
+        this.setMaxDamage(1000); //1000 millibuckets = 1 bucket
 	}
 	
+	@Override
     public EnumAction getItemUseAction(ItemStack par1ItemStack)
     {
         return EnumAction.bow;
+    }
+    
+    @Override
+    public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        return par1ItemStack;
+    }
+    
+    @Override
+    public int getMaxItemUseDuration(ItemStack par1ItemStack)
+    {
+        return 36000;
+    }
+    
+    @Override
+    public int getItemEnchantability()
+    {
+        return 0; //nope, no enchants
+    }
+    
+    @Override
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        if (par3EntityPlayer.capabilities.isCreativeMode || par1ItemStack.getItemDamage() < 1000)
+        {
+            par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+        }
+
+        return par1ItemStack;
+    }
+    
+    @Override
+    public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4)
+    {
+        boolean flag = par3EntityPlayer.capabilities.isCreativeMode;
+
+        if (flag || par1ItemStack.getItemDamage() < 1000) //if gamemode is creative, or if we still have some liquid left...
+        {
+            par1ItemStack.damageItem(1, par3EntityPlayer);
+        }
     }
 }
